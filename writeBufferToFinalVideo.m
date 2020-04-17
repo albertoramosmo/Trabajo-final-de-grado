a9a64abf6db62824e257e87f53cda0f1151caf43
albertoramosmo <63864489+albertoramosmo@users.noreply.github.com>
2020-04-17 18:01:58 +0100

    Add files via upload

---
+++ writeBufferToFinalVideo.m
@@ -0,0 +1,14 @@
+function [video] = writeBufferToFinalVideo(buffer)
+video = VideoWriter('snailNuevo','MPEG-4'); 
+video.FrameRate = 25; 
+open(video)
+
+for i = 1:size(buffer,4)    
+    img = buffer(:,:,:,i); 
+    img(img>1) = 1;                           % Establecemos los límites para que el
+    img(img<0) = 0; 
+    writeVideo(video,img); 
+end
+close(video);
+end
+
