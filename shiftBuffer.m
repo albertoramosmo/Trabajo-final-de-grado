1a998b9890af06de5be3edbdbf83313153ef181b
albertoramosmo <63864489+albertoramosmo@users.noreply.github.com>
2020-04-20 18:01:08 +0100

    Delete shiftBuffer.m

--- shiftBuffer.m
+++
@@ -1,9 +0,0 @@
-function frameBuffer = shiftBuffer (frameBuffer,frame,framesInBuffer)
-% Es una cola FIFO de frames sobre la que actuas. ShiftBuffer es básicamente 
-% hacer un desplazamiento de los elementos (o mover un puntero).                
-frameBuffer(:,:,:,framesInBuffer) = frame;
-end
-
-
-
-
