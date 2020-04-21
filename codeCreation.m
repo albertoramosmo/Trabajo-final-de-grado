d5ffd21ff58217fdb19b224e1242591295a3ccaf
albertoramosmo <63864489+albertoramosmo@users.noreply.github.com>
2020-04-21 17:43:11 +0100

    Add files via upload

---
+++ codeCreation.m
@@ -0,0 +1,10 @@
+function [code] = codeCreation(codeRows,codeCols)
+codeSize=codeCols*codeRows;
+% Creamos el code entre 0 y 1 con un tamaño de codeSize.
+code = randi([0, 1], [1, codeSize]);
+
+% Lo convertimos a -1 y 1.
+code(code>1)   = 1.0;                           
+code(code<=0)  = -1.0; 
+end
+
